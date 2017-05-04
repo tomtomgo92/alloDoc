8590b129e9a4ce9fbf20b992917db044e841421b
mac <mac@ios11-toma.afpa-paris-di>
2017-05-04 14:55:48 +0200

    shadow

--- alloDoc/customButton.swift
+++ alloDoc/customButton.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class customButton: UIButton
 {
+
+    
     
     func radius(_ radius: Double)
     {
@@ -102,13 +104,17 @@ class customButton: UIButton
 
     func ombre()
     {
+        let radius: CGFloat = self.frame.width / 2.0 //change it to .height if you need spread for height
+        let shadowPath = UIBezierPath(rect: CGRect(x: 0, y: 0, width: 2.1 * radius, height: self.frame.height))
+        //Change 2.1 to amount of spread you need and for height replace the code for height
+        
         self.layer.cornerRadius = 2
         self.layer.shadowColor = UIColor.black.cgColor
         self.layer.shadowOffset = CGSize(width: 0.5, height: 0.4)  //Here you control x and y
         self.layer.shadowOpacity = 0.5
         self.layer.shadowRadius = 5.0 //Here your control your blur
         self.layer.masksToBounds =  false
-        
+        self.layer.shadowPath = shadowPath.cgPath
     }
     
     func btmInsets()
